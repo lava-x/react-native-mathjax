8aec4712a931dd7157221b04401f6e6e2ea7b727
hslavax <hengsheng@lavax.co>
2021-11-11 01:47:01 +0800

    add support to font family and font size for webview

--- index.js
+++ index.js
@@ -1,41 +1,77 @@
-import React from 'react';
-import { View } from 'react-native';
-import { WebView } from 'react-native-webview';
+import React from "react";
+import { View } from "react-native";
+import { WebView } from "react-native-webview";
 
 const defaultOptions = {
-	messageStyle: 'none',
-	extensions: ['tex2jax.js'],
-	jax: ['input/TeX', 'output/HTML-CSS'],
-	tex2jax: {
-		inlineMath: [['$', '$'], ['\\(', '\\)']],
-		displayMath: [['$$', '$$'], ['\\[', '\\]']],
-		processEscapes: true,
-	},
-	TeX: {
-		extensions: ['AMSmath.js', 'AMSsymbols.js', 'noErrors.js', 'noUndefined.js']
-	}
+  messageStyle: "none",
+  extensions: ["tex2jax.js"],
+  jax: ["input/TeX", "output/HTML-CSS"],
+  tex2jax: {
+    inlineMath: [
+      ["$", "$"],
+      ["\\(", "\\)"],
+    ],
+    displayMath: [
+      ["$$", "$$"],
+      ["\\[", "\\]"],
+    ],
+    processEscapes: true,
+  },
+  TeX: {
+    extensions: [
+      "AMSmath.js",
+      "AMSsymbols.js",
+      "noErrors.js",
+      "noUndefined.js",
+    ],
+  },
+  fontFamily: "san-serif",
+  fontSize: 12,
 };
 
 class MathJax extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			height: 1
-		};
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      height: 1,
+    };
+  }
 
-	handleMessage(message) {
-		this.setState({
-			height: Number(message.nativeEvent.data)
-		});
-	}
+  generateAssetsFontCss(fontFamily, fontSize) {
+    const fileUri = Platform.select({
+      ios: `${fontFamily}.ttf`,
+      android: `file:///android_asset/fonts/${fontFamily}.ttf`,
+    });
 
-	wrapMathjax(content) {
-		const options = JSON.stringify(
-			Object.assign({}, defaultOptions, this.props.mathJaxOptions)
-		);
+    return `
+		@font-face {
+			font-family: '${fontFamily}';
+			src: local('${fontFamily}'), url('${fileUri}') format('truetype');
+		}
 
-		return `
+		body {
+			font-family: 'OpenSans-Regular';
+			font-size: ${fontSize}px;
+		}
+		`;
+  }
+
+  handleMessage(message) {
+    this.setState({
+      height: Number(message.nativeEvent.data),
+    });
+  }
+
+  wrapMathjax(content) {
+    const { fontFamily, fontSize, mathJaxOptions } = this.props;
+
+    const options = JSON.stringify(
+      Object.assign({}, defaultOptions, mathJaxOptions)
+    );
+
+    const css = this.generateAssetsFontCss(fontFamily, fontSize);
+
+    return `
 			<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 			<script type="text/x-mathjax-config">
 				MathJax.Hub.Config(${options});
@@ -46,30 +82,31 @@ class MathJax extends React.Component {
 					document.getElementById("formula").style.visibility = '';
 				});
 			</script>
-
+			<style type="text/css">${css}</style>
 			<script src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.0/MathJax.js"></script>
 			<div id="formula" style="visibility: hidden;">
 				${content}
 			</div>
 		`;
-	}
-	render() {
-		const html = this.wrapMathjax(this.props.html);
+  }
+  render() {
+    const html = this.wrapMathjax(this.props.html);
 
-		// Create new props without `props.html` field. Since it's deprecated.
-		const props = Object.assign({}, this.props, { html: undefined });
+    // Create new props without `props.html` field. Since it's deprecated.
+    const props = Object.assign({}, this.props, { html: undefined });
 
-		return (
-			<View style={{ height: this.state.height, ...props.style }}>
-				<WebView
-					scrollEnabled={false}
-					onMessage={this.handleMessage.bind(this)}
-					source={{ html }}
-					{...props}
-				/>
-			</View>
-		);
-	}
+    return (
+      <View style={{ height: this.state.height, ...props.style }}>
+        <WebView
+          scrollEnabled={false}
+          onMessage={this.handleMessage.bind(this)}
+          source={{ html, baseUrl: "" }}
+          originWhitelist={["*"]}
+          {...props}
+        />
+      </View>
+    );
+  }
 }
 
 export default MathJax;
